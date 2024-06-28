63a1a80d655d6262b0c71cde610114b3cd85b253
srrifat <srrifat781@gmail.com>
2024-06-28 18:56:35 +0600

    input in java

---
+++ input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class input {    
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("You entered: " +name);
+        sc.close();
+    }
+}
