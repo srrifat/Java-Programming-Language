99eeac388c41b1cf155fb8852563fb556316320e
srrifat <srrifat781@gmail.com>
2024-06-30 21:49:24 +0600

    sum in A & B

--- input.java
+++ input.java
@@ -5,6 +5,11 @@ public class input {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine();
         System.out.println("You entered: " +name);
+        int numebr =sc.nextInt();
+        System.out.println("You entered: "+numebr);
+
+        float price = sc.nextFloat();
+        System.out.println("You entered: "+price);   
         sc.close();
     }
 }
