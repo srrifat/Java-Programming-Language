7541967ecaa11ba6c083c79b4ee00480b62ee9ef
srrifat <srrifat781@gmail.com>
2024-12-11 08:54:46 +0600

    Pairs in an Array

--- reverseArray.java
+++ reverseArray.java
@@ -1,19 +1,29 @@
 import java.util.*;
-//Reverse an Array 
+
+// Reverse an Array
 public class reverseArray {
-    public static void reverse(int numbers[]{
-        
-  int first = 0, last = numbers.length-1;
-  while(first < last){
-    //swap
-    int temp = numbers[last];
-    numbers[last] = numbers[first];
-    numbers[first] = temp;
 
+    // Method to reverse the array
+    public static void reverse(int numbers[]) {
+        int first = 0, last = numbers.length - 1;
+        while (first < last) {
+            // Swap
+            int temp = numbers[last];
+            numbers[last] = numbers[first];
+            numbers[first] = temp;
 
-  }
-}
-    public static void main(String[] args) {
+            // Move the pointers
+            first++;
+            last--;
+        }
+    }
+
+    public static void main(String args[]) {
         int numbers[] = {2, 4, 6, 8, 10};
+        reverse(numbers); // Reverse the array
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " "); // Print the reversed array
+        }
+        System.out.println();
     }
 }
